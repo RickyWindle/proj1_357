36220139a2e0caf17d568c4e9512ec595697e5e2
RickyWindle <kyongwindle@gmail.com>
2014-02-18 20:33:50 -0600

    tau_dcmotor
    ver.2

--- tau_dcmotor.m
+++ tau_dcmotor.m
@@ -3,19 +3,21 @@ function [ tau ] = tau_dcmotor( omega, motor )
 %given the shaft speed in rad/s and the motor specifications 
 % omegacheck=isnumeric(omega)
 % omegasize=length(omega)
-if nargin(tau_dcmotor) ~= 2
+if nargin ~= 2
     error('Check number of inputs')
 elseif isnumeric(omega) ~= 1
     error('Omega is not a numeric array')
 elseif isstruct(motor)~=1
     error('Motor is not a structure array')
+else
+    if omega>motor.speed_noload
+        tau=0;
+    elseif omega < 0
+        tau = motor.torque_stall;
+    else
+        tau=motor.torque_stall-(motor.torque_stall-motor.torque_noload)*...
+        omega/motor.speed_noload;
+    end
 end
-if omega>motor.speed_noload
-    tau=0;
-elseif omega < 0
-    tau = motor.torque_stall;
-end
-tau=motor.torque_stall-(motor.torque_stall-motor.torque_noload)*...
-    omega/motor.speed_noload;
 end
 
