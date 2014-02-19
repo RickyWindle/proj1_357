80ff96f4b3c2258e0710d26c91e8b64dee4d9f22
RickyWindle <kyongwindle@gmail.com>
2014-02-18 23:13:48 -0600

    All function files updated
    see title

--- F_gravity.m
+++ F_gravity.m
@@ -5,13 +5,19 @@ if nargin ~= 3
     error('Check number of inputs');
 elseif isnumeric(terrain_angle)~=1
     error('Speed reducer is not a numeric array');
-elseif terrain_angle<-75 || terrain_angle>75
+elseif min(terrain_angle)<-75 || max(terrain_angle)>75
     error('Terrain_angle is not practical');
 elseif isstruct(rover)~=1
     error('Rover is not a structure array');
 elseif isstruct(planet)~=1
     error('Planet is not a structure array');
-else Fgt=get_mass(rover)*planet.g*sind(terrain_angle);
+else
+    i=max(size(terrain_angle));
+    F=zeros(1,i);
+    for n=1:i
+        F(n)=get_mass(rover)*planet.g*sind(terrain_angle(n));
+    end
+    Fgt=sum(F);
 end
 
 end
--- F_rolling.m
+++ F_rolling.m
@@ -2,19 +2,28 @@ function [ Frr ] = F_rolling( terrain_angle, rover, Crr, planet, omega_motor )
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
 
-if nargin ~= 2
-    error('Check number of inputs')
-elseif isnumeric(omega_motor) || isnumeric(terrain_angle) ~= 1
-    error('Omega_motor and/or terrain_angle is not a numeric array')
+if nargin ~= 5
+    error('Check number of inputs');
+elseif isnumeric(omega_motor)~=1 || isnumeric(terrain_angle) ~= 1
+    error('Omega_motor and/or terrain_angle is not a numeric array');
 elseif isstruct(rover)~=1
     error('Rover is not a structure array')
-elseif terrain_angle <75 || terrain_angle <-75
+elseif max(terrain_angle) >75 || min(terrain_angle) <-75
     error('Terrain angle is not practicle');
 elseif Crr<0
     error('Crr is negative');
 else
-    
-    Frr=erf(40*vr)*Crr*get_mass(rover)*planet.g*cosd(terrain_angle)
- 
+    i=max(size(omega_motor));
+    omega_out=zeros(1,i);
+    vr=zeros(1,i);
+    F=zeros(1,i);
+    for n=1:i
+        omega_out(n)=omega_motor(n)/get_gear_ratio(rover.wheel_assembly.speed_reducer);
+        vr(n)=omega_out(n)*rover.wheel_assembly.wheel.radius;
+        F(n)=erf(40*vr(n))*Crr*get_mass(rover)*planet.g*cosd(terrain_angle(n));
+    end
+    Frr=sum(F);
+end
+
 end
 
